f0f851cd04af48f7a427a91b57b24bc2298acd63
voxvanhieu <voxvanhieu@gmail.com>
2020-04-05 18:11:34 +0700

    Config app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Extra Classroom'),
+    'subtitle' => 'One place for your class!',
 
     /*
     |--------------------------------------------------------------------------
