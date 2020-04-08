c7d669804d2e6a8fed4f9fd70737de3994cf014d
voxvanhieu <voxvanhieu@gmail.com>
2020-04-08 16:35:59 +0700

    Change teacher/student controller

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -14,6 +14,6 @@ class TeacherController extends Controller
      */
     public function teacherDashboard()
     {
-        return view('teacherDashboard');
+        return view('teacher.index');
     }
 }
