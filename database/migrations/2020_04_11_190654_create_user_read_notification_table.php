617e141210700f13fa9f6ef73245494efa1330fb
Duong Vo <duongvox99@gmail.com>
2020-04-11 19:22:31 +0700

    UPDATE database
    add user_read_notification
    edit relation between user and group
    rename pivot table: user_answer

--- database/migrations/2020_04_11_190654_create_user_read_notification_table.php
+++ database/migrations/2020_04_11_190654_create_user_read_notification_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserNotificationsTable extends Migration
+class CreateUserReadNotificationTable extends Migration
 {
     /**
      * Run the migrations.
