cd5f437111b35188e37be0286f0733e7d1539341
Duong Vo <duongvox99@gmail.com>
2020-04-27 00:22:30 +0700

    Complete CRUD Topic

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Topic;
+use App\TypeClass;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TopicController extends Controller
 {
     private $customMessages = [
             'name.required' => 'Tên không được rỗng',
             'class.required'  => 'Lớp không được rỗng',
-            'name.unique' => 'Tên đã tồn tại',
             'name.max' => 'Tên phải ít hơn 255 ký tự'
         ];
 
@@ -21,7 +22,20 @@ class TopicController extends Controller
      */
     public function index()
     {
-        $topics = Topic::where('id', '>', 1)->get();
+        $topics = DB::table('topics')
+            ->join('type_classes', 'type_classes.id', '=', 'topics.type_class_id')
+            ->select([
+                'topics.id as topicId',
+                'topics.name as topicName',
+                'topics.class',
+                'description',
+                'topics.updated_at as topicUpdatedAt',
+                'type_classes.name as typeClassName'])
+            ->orderBy('topics.updated_at', 'desc')
+//            ->orderBy('topics.class', 'desc')
+//            ->orderBy('type_classes.name', 'desc')
+            ->orderBy('topics.name', 'asc')
+            ->get();
         return view('teacher.topics.index', compact('topics'));
     }
 
@@ -32,7 +46,8 @@ class TopicController extends Controller
      */
     public function create()
     {
-        return view('teacher.topics.create');
+        $typeClasses = TypeClass::all(['id', 'name']);
+        return view('teacher.topics.create', compact('typeClasses'));
     }
 
     /**
@@ -44,11 +59,16 @@ class TopicController extends Controller
     public function store(Request $request)
     {
         $validator = $request->validate([
-            'name' => 'required|unique:topics|max:255',
+            'name' => 'required|max:255',
             'class' => 'required',
         ], $this->customMessages);
 
-        Topic::create($request->all());
+        Topic::create([
+            'name' => $request->name,
+            'type_class_id' => $request->typeOfClassId,
+            'class' => $request->class,
+            'description' => $request->description
+        ]);
 
         return redirect()->route('teacher.topics.index')->with('isStored', true);
     }
@@ -61,7 +81,8 @@ class TopicController extends Controller
      */
     public function show(Topic $topic)
     {
-        //
+        $typeClasses = TypeClass::all(['id', 'name']);
+        return view('teacher.topics.show', compact(['topic', 'typeClasses']));
     }
 
     /**
@@ -72,11 +93,8 @@ class TopicController extends Controller
      */
     public function edit(Topic $topic)
     {
-        if ($topic->id == 1)
-        {
-            abort(404);
-        }
-        return view('teacher.topics.edit', compact('topic'));
+        $typeClasses = TypeClass::all(['id', 'name']);
+        return view('teacher.topics.edit', compact(['topic', 'typeClasses']));
     }
 
     /**
@@ -92,7 +110,12 @@ class TopicController extends Controller
             'name' => 'required|max:255',
             'class' => 'required',
         ], $this->customMessages);
-        $topic->update($request->all());
+        $topic->update([
+            'name' => $request->name,
+            'type_class_id' => $request->typeOfClassId,
+            'class' => $request->class,
+            'description' => $request->description
+        ]);
 
         return redirect()->route('teacher.topics.index')->with('isUpdated', true);
     }
